78d7bbca40a48f67084a9287803cb08175f0f694
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2018-01-05 22:05:25 -0600

    style test

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
 <br><br>
 
 <div class="container" id="cborder">
-	<div class="row">
+	<div class="row" id="cborder">
 		<div class="col-xs-12 col-md-12 col-lg-12">
 			<center><h3><u>Calender</u></h3></center>
 			<br><br>
 		</div>
 	</div>
-	<div class="row">
+	<div class="row" id="cborder">
 		<center>
 		<div class="col-xs-2 col-md-2 col-lg-2">
 			<center><p>##</p></center>
