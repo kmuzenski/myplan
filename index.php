17f0404ca03617e8dfc4cb8ab125ca88ab789f19
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2018-01-05 21:48:12 -0600

    testing boot strap + style

--- index.php
+++ index.php
@@ -3,6 +3,40 @@
 <?php require_once('header.php'); ?>
 <body>
 <h1>myPlan</h1>
+<br><br>
+
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-md-12 col-lg-12">
+			<h3><u>Calender</u></h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-1 col-md-1 col-lg-1">
+			<p>##</p>
+		</div>
+		<div class="col-xs-1 col-md-1 col-lg-1">
+			<p>##</p>
+		</div>
+		<div class="col-xs-1 col-md-1 col-lg-1">
+			<p>##</p>
+		</div>
+		<div class="col-xs-1 col-md-1 col-lg-1">
+			<p>##</p>
+		</div>
+		<div class="col-xs-1 col-md-1 col-lg-1">
+			<p>##</p>
+		</div>
+		<div class="col-xs-1 col-md-1 col-lg-1">
+			<p>##</p>
+		</div>
+		<div class="col-xs-1 col-md-1 col-lg-1">
+			<p>##</p>
+		</div>
+	</div>
+
+
+
 <?php require_once('footer.php'); ?>
 </body>
 </html>
