436bd2c4c6b43714884c0a0cfe4d380067439c1d
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2018-01-05 21:29:05 -0600

    + header + footer

---
+++ header.php
@@ -0,0 +1,4 @@
+<head>
+<link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="assets/css/styles.css" rel="stylesheet" type="text/css">
+</head>
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
-
+<?php require_once('header.php'); ?>
 <body>
 <h1>myPlan</h1>
+<?php require_once('footer.php'); ?>
 </body>
 </html>
