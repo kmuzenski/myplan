af697e4953937bdccd66157bec63e92f0e4efa75
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2018-01-05 22:40:57 -0600

    style

--- index.php
+++ index.php
@@ -16,23 +16,64 @@
 		<center>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<p>1.</p>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<p>2.</p>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<p>3.</p>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<p>4.</p>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<p>5.</p>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
-			<p>6.</p>
-			<hr>
-			<p>7.</p>
+			<p>6. / 7.</p>
+			<a href="newevent.php">+ event</a>
+			<br>			
+		</div>
+	</div>
+	<div class="row" id="cborder">
+		<center>
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
+			<left><p>8.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
+		</div>
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
+			<left><p>8.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
+		</div>
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
+			<left><p>9.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
+		</div>
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
+			<left><p>10.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
+		</div>
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
+			<left><p>11.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
+		</div>
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
+			<left><p>12.</p></left>
 			<a href="newevent.php">+ event</a>
 			<br>
 		</div>
@@ -40,85 +81,100 @@
 	<div class="row" id="cborder">
 		<center>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
-			<left><p>7.</p></left>
-		</div>
-		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
-			<left><p>8.</p></left>
-		</div>
-		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
-			<left><p>9.</p></left>
-		</div>
-		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
-			<left><p>10.</p></left>
-		</div>
-		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
-			<left><p>11.</p></left>
-		</div>
-		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
-			<left><p>12.</p></left>
-		</div>
-	</div>
-	<div class="row" id="cborder">
-		<center>
-		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>13.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>14.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>15.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>16.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>17.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>18.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 	</div>
 	<div class="row" id="cborder">
 		<center>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>19.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>20.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>21.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>22.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>23.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>24.</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 	</div>
 	<div class="row" id="cborder">
 		<center>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>##</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>##</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>##</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>##</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>##</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<left><p>##</p></left>
+			<a href="newevent.php">+ event</a>
+			<br>
 		</div>
 	</div>
 	
