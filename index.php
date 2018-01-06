a88aa60a3ab4d132646f329cf9f133628e2a803b
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2018-01-05 22:07:20 -0600

    style test

--- index.php
+++ index.php
@@ -8,28 +8,28 @@
 <div class="container" id="cborder">
 	<div class="row" id="cborder">
 		<div class="col-xs-12 col-md-12 col-lg-12">
-			<center><h3><u>Calender</u></h3></center>
+			<center><h2><u>Calender</u></h2></center>
 			<br><br>
 		</div>
 	</div>
 	<div class="row" id="cborder">
 		<center>
-		<div class="col-xs-2 col-md-2 col-lg-2">
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<center><p>##</p></center>
 		</div>
-		<div class="col-xs-2 col-md-2 col-lg-2">
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<center><p>##</p></center>
 		</div>
-		<div class="col-xs-2 col-md-2 col-lg-2">
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<center><p>##</p></center>
 		</div>
-		<div class="col-xs-2 col-md-2 col-lg-2">
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<center><p>##</p></center>
 		</div>
-		<div class="col-xs-2 col-md-2 col-lg-2">
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<center><p>##</p></center>
 		</div>
-		<div class="col-xs-2 col-md-2 col-lg-2">
+		<div class="col-xs-2 col-md-2 col-lg-2" id="cborder">
 			<center><p>##</p></center>
 		</div>
 		
